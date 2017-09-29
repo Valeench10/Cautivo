904468e57c1e91228c4c9ff41b88895f957be03e
Valeench10 <32395262+Valeench10@users.noreply.github.com>
2017-09-29 11:02:23 -0500

    Subo el proyecto inicial
    Subo los assets iniciales para el proyecto Cautivo.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public InputField field;
+    public string nombre;
+    public Text texto;
+
+    public void SayHello()
+    {
+        nombre = field.text;
+        texto.text = "Holi " + nombre;
+    }
+   
+}
